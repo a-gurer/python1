36b7047e6ec0eb9763f0da9165d70a7ad72c1b52
a-gurer <asli.gurer1@gmail.com>
2017-05-01 17:35:16 -0400

    Add files via upload

---
+++ 1stclass.py
@@ -0,0 +1,15 @@
+
+def functionName(myParameter1, myParameter2) :
+    myVar2=5
+    print(myParameter1)
+    print(myParameter2)
+myVar=5
+print(myVar)
+
+functionName(1,2)
+
+def myAvg(a1,a2,a3,a4,a5):
+    mysum=a1+a2+a3+a4+a5
+    print(mysum/5)
+
+myAvg(1,2,3,4,5)
